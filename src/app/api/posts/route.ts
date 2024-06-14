70a73cf3931d56b1a7d07625f67ce15bb41855fd
Ashot Israelyan <ashot.israelyan@tumo.org>
2024-06-14 17:26:39 +0400

    feat: some changes

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,7 +1,44 @@
 import prisma from '@/lib/prisma';
 
 export async function GET() {
-	const posts = await prisma.post.findMany();
+	// const posts = await prisma.post.findMany({
+	// 	where: {
+	// 		OR: [
+	// 			{
+	// 				title: {
+	// 					contains: 'github',
+	// 					mode: 'insensitive'
+	// 				}
+	// 			},
+	// 			{
+	// 				title: {
+	// 					contains: 'Twitter'
+	// 				}
+	// 			}
+	// 		],
+	// 		AND: {
+	// 			published: true,
+	// 		}
+	// 	}
+	// });
+
+	const posts = await prisma.post.findMany({
+		where: {
+			author: {
+				isNot: {
+					name: 'Jack'
+				},
+				is: {
+					email: {
+						startsWith: 's'
+					}
+				}
+			}
+		},
+		include: {
+			author: true,
+		}
+	});
 
 	return new Response(JSON.stringify(posts));
 }
